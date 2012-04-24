00605f9db36498a8f93693c85dee787f97230536
Joseph Chiang <joseph_chiang@miiicasa.com>
2012-04-24 18:08:52 +0800

    [Ref #3027] Update translation string.

--- lang/editable_en-US.js
+++ lang/editable_en-US.js
@@ -1,11 +1,12 @@
+/*global YUI */
 YUI.add("lang/editable_en-US", function (Y) {
     Y.Intl.add("editable", "en-US", {
         default_empty_hint: "Please input your text...",
-        default_error_message: "Something wrong, please try again.",
-        require_error_message: "Your input can't be empty.",
-        max_length_error_message: "Your input exceeds max length.",
-        min_length_error_message: "Your input is less than min length.",
-        filename_error_message: "The filename you input is illegal.",
+        default_error_message: "Some error happen, please try again.",
+        require_error_message: " The field cannot be blank.",
+        max_length_error_message: "Your input contains too many characters.",
+        min_length_error_message: "Your input is less than minimal length.",
+        filename_error_message: 'These characters cannot be used: &#92; &#47; &#58; &#42; &#63; &#34; &#60; &#62; &#124;',
         default_tooltip: "Click here to edit...",
         header_prompt: "Edit",
         save_label: "Save",
--- lang/editable_zh-TW.js
+++ lang/editable_zh-TW.js
@@ -1,15 +1,15 @@
+/*global YUI */
 YUI.add("lang/editable_zh-TW", function (Y) {
     Y.Intl.add("editable", "zh-TW", {
         default_empty_hint: "請輸入文字...",
-        default_error_message: "很抱歉，你的輸入有誤，請修改再試一次",
-        require_error_message: "此欄位為必填",
-        max_length_error_message: "您的輸入超過最大限制",
+        default_error_message: "有錯誤狀況發生，請再試一次。",
+        require_error_message: "本欄位不能空白。",
+        max_length_error_message: "你所輸入的文字超過限制。",
         min_length_error_message: "您的限制小於最小限制",
-        filename_error_message: "您的檔案名稱不符合規範",
-        default_tooltip: "點選即可修改此處資料",
+        filename_error_message: '下列字元不能使用：&#92; &#47; &#58; &#42; &#63; &#34; &#60; &#62; &#124;',
+        default_tooltip: "點選即可編輯",
         header_prompt: "修改資料",
         save_label: "儲存",
-        cancel_label: "放棄"
+        cancel_label: "取消"
     });
 });
-
